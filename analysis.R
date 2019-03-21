23d293d584380dd9122ea5b1b1530508a1625ef1
thebigez8 <thebigez8@gmail.com>
2019-03-20 23:21:56 -0500

    Update analysis

--- analysis.R
+++ analysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(arsenal)
 
 impute <- function(d)
 {
@@ -13,6 +14,16 @@ impute <- function(d)
   d
 }
 
+partial.tanks <- function(d)
+{
+  for(u in which(d$mpg > 100))
+  {
+    d$mpg[u + 0:1] <- mean(d$mpg[u + 0:1])
+    d$smoothed[u + 0:1] <- TRUE
+  }
+  d
+}
+
 dat <- "mpg.csv" %>%
   read_csv(col_types = cols(), col_names = TRUE, na = "?") %>%
   mutate(
@@ -21,13 +32,16 @@ dat <- "mpg.csv" %>%
     i = 1:n()
   ) %>%
   impute() %>%
-  arrange(is.na(Gallons) + (Notes %nin% c("Bought", "unk")), Date) %>%
+  arrange(is.na(Gallons) & (Notes %nin% c("Bought", "unk")), Date) %>%
   mutate(
     miles = if_else(is.na(Gallons), NA_real_, c(NA_real_, diff(Mileage))),
     mpg = miles / Gallons,
-    ppg = Price / Gallons
+    ppg = Price / Gallons,
+    Break = if_else(Date >= as.Date("2017-01-08"), "After", "Before"),
+    smoothed = FALSE
   ) %>%
-  arrange(i)
+  arrange(i) %>%
+  partial.tanks()
 
 stopifnot(diff(dat$Date) >= 0)
 stopifnot(diff(dat$Mileage) > 0)
@@ -38,13 +52,16 @@ stopifnot(all(dat$Price > 0, na.rm = TRUE))
 p1 <- dat %>%
   filter(!is.na(mpg)) %>%
   ggplot(aes(x = Date, y = mpg)) +
-  geom_point() +
   geom_line() +
-  geom_hline(yintercept = median(dat$mpg, na.rm = TRUE))
+  geom_point(aes(color = Break, shape = smoothed)) +
+  geom_hline(yintercept = median(dat$mpg, na.rm = TRUE)) +
+  theme(legend.position = 'none') +
+  scale_shape_manual(values = c("TRUE" = 15, "FALSE" = 16))
 
 p2 <- ggplot(dat, aes(x = Date, y = Mileage)) +
-  geom_point() +
-  geom_line()
+  geom_line() +
+  geom_point(aes(color = Break)) +
+  theme(legend.position = 'none')
 
 p3 <- dat %>%
   filter(!is.na(ppg)) %>%
